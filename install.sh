ff6a853488a4da451d3d9f1403f835cbd0ef87f1
Cole Eason <cole@expensify.com>
2020-06-19 17:51:13 -0700

    Fix install linking of vagrant folder

--- install.sh
+++ install.sh
@@ -2,12 +2,24 @@
 
 DIR="$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-for item in $(ls -d .??* | grep -v -x .git); do
+for item in $(ls -d .??* | grep -v -x .git | grep -v -x .vagrant.d); do
     ln -snf $DIR/$item ~
 done
 
+if [[ ! -d ~/.git/ ]]; then
+	mkdir -p ~/.git/hooks
+fi
+
 for hook in hooks/*; do
     ln -snf $DIR/$hook ~/.git/hooks/
+done
+
+if [[ ! -d ~/.vagrant.d/ ]]; then
+	mkdir -p ~/.vagrant.d
+fi
+
+for file in .vagrant.d/*; do
+	ln -snf $DIR/$file ~/.vagrant.d/
 done
 
 echo "Done."

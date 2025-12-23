5931a3b17026c33864cc6d946505851985d39bea
Cole Eason <cole@expensify.com>
2025-12-23 11:16:08 -0600

    Make nvim lazy plugin config work, add diagnostics

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -10,39 +10,84 @@ return {
             vim.cmd([[colorscheme catppuccin-mocha]])
         end, 
     },
-    { 
-		"neovim/nvim-lspconfig",
-		config = function(server, opts)
-            for server, settings in pair(opts.servers) do
-                vim.lsp.config(server, settings)
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local servers = {
+                rust_analyzer = {
+                    settings = {
+                    ["rust-analyzer"] = {
+                        imports = {
+                            granularity = {
+                                group = "module",
+                            },
+                            prefix = "self",
+                        },
+                        cargo = {
+                            buildScripts = {
+                                enable = true,
+                            },
+                        },
+                        procMacro = {
+                            enable = true
+                        },
+                        check = {
+                            command = "clippy"
+                        },
+                    },
+                },
+                    on_attach = function(client, bufnr)
+                        vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+                    end
+            }
+        }
+
+            -- Load all of the servers in the above servers array
+            for server, opts in pairs(servers) do
+                -- opts.on_attach = function(client, bufnr)
+                --     vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+                -- end,
+    
+                vim.lsp.config[server] = opts
                 vim.lsp.enable(server)
             end
         end,
-		opts = function()
-            local servers = { 
-                rust_analyzer = { settings = {
-            --     imports = {
-            --         granularity = {
-            --             group = "module",
-            --         },
-            --     prefix = "self",
-            --     },
-            --     cargo = {
-            --         buildScripts = {
-            --             enable = true,
-            --         },
-            --     },
-            --     procMacro = {
-            --         enable = true
-            --     },
-            -- },
-        },
-            -- inlay_hints = {
-            --    enabled = true,
-            --},
-        },
-        },
-            return servers
-        end,
     },
+    {
+  "folke/trouble.nvim",
+  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  cmd = "Trouble",
+  keys = {
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>xX",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<leader>cs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>cl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>xL",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<leader>xQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
+  },
+}
 }
